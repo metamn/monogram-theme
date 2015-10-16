796ea71320bf3ce4d96b27645594e6004defc494
cs <bartus.csongor@gmail.com>
2015-10-16 13:38:59 +0300

    Random spaces inserted into the monogram

--- site/components/project/monogram/monogram.js
+++ site/components/project/monogram/monogram.js
@@ -1,1 +1,16 @@
+var insertSpace = function(containerID) {
+  var container = document.querySelector(containerID);
+  var text = container.innerHTML;
+  // How many spaces to insert
+  var spaces = Math.round(text.length / 3);
+
+  for (var i = 0; i <= spaces; i++) {
+    var random = Math.floor((Math.random() * (text.length - 1)));
+    text = text.slice(0, random) + " " + text.slice(random);
+  }
+
+  container.innerHTML = text;
+}
+
+insertSpace('.monogram .title');
 setBackgroundImage('.monogram');
