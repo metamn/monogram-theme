4eea9111b8b5ce7a7d233d8bba6a11295fd81c29
cs <bartus.csongor@gmail.com>
2015-10-18 10:56:15 +0300

    Cube is well rotating randomly

--- site/components/project/cube/cube.js
+++ site/components/project/cube/cube.js
@@ -2,30 +2,27 @@ var cubeRotate = function(containerID) {
   var container = document.querySelector(containerID);
   var xAngle = 0, yAngle = 0;
 
-  document.addEventListener('keydown', function(e) {
-    switch(e.keyCode) {
+  var random = Math.floor(Math.random() * 4000);
+  var direction = random % 4;
 
-      case 37: // left
-        yAngle -= 90;
-        break;
+  switch(direction) {
+    case 0: // left
+      yAngle -= 90;
+      break;
+    case 1: // up
+      xAngle += 90;
+      break;
+    case 2: // right
+      yAngle += 90;
+      break;
+    case 3: // down
+      xAngle -= 90;
+      break;
+  };
 
-      case 38: // up
-        xAngle += 90;
-        break;
+  transform(container, "rotateX(" + xAngle + "deg) rotateY(" + yAngle + "deg)");
 
-      case 39: // right
-        yAngle += 90;
-        break;
-
-      case 40: // down
-        xAngle -= 90;
-        break;
-    };
-
-    transform(container, "rotateX(" + xAngle + "deg) rotateY(" + yAngle + "deg)");
-  }, false);
-
-
+  // Cross browser CSS transform
   function transform(container, style) {
     container.style.webkitTransform = style;
 
@@ -36,4 +33,10 @@ var cubeRotate = function(containerID) {
   }
 }
 
-cubeRotate('.cube3d');
+
+var repeat = setInterval(
+  function(){
+    cubeRotate('.cube3d');
+  },
+  2000
+);
