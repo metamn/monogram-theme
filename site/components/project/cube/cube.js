688b194610238a3ca2dc32b4b96e2bec4ba97ffa
cs <bartus.csongor@gmail.com>
2015-11-03 15:49:03 +0200

    Moving back to CSS

--- site/components/project/cube/cube.js
+++ site/components/project/cube/cube.js
@@ -1,41 +1,28 @@
 var scene, camera, renderer;
 
-var WIDTH  = window.innerWidth;
-var HEIGHT = window.innerHeight;
+var WIDTH  = 300;
+var HEIGHT = 300;
 
 var SPEED = 0.01;
 
 var cubeInit = function(containerID) {
   scene = new THREE.Scene();
 
-  initCube();
-  initCamera();
-  initRenderer();
+  // init cube
+  cube = new THREE.Mesh(new THREE.CubeGeometry(2, 2, 2), new THREE.MeshNormalMaterial());
+  scene.add(cube);
+
+  // init camera
+  camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 10);
+  camera.position.set(0, 3.5, 5);
+  camera.lookAt(scene.position);
+
+  // init renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(WIDTH, HEIGHT);
 
   var container = document.querySelector(containerID);
   container.appendChild(renderer.domElement);
-}
-
-function initCamera() {
-    camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 10);
-    camera.position.set(0, 3.5, 5);
-    camera.lookAt(scene.position);
-}
-
-function initRenderer() {
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(WIDTH, HEIGHT);
-}
-
-function initCube() {
-    cube = new THREE.Mesh(new THREE.CubeGeometry(2, 2, 2), new THREE.MeshNormalMaterial());
-    scene.add(cube);
-}
-
-function rotateCube() {
-    cube.rotation.x -= SPEED * 2;
-    cube.rotation.y -= SPEED;
-    cube.rotation.z -= SPEED * 3;
 }
 
 
@@ -43,6 +30,12 @@ var cubeRender = function() {
   requestAnimationFrame(cubeRender);
   rotateCube();
   renderer.render(scene, camera);
+
+  function rotateCube() {
+    cube.rotation.x -= SPEED * 2;
+    cube.rotation.y -= SPEED;
+    cube.rotation.z -= SPEED * 3;
+  }
 }
 
 cubeInit('.cube__container');
