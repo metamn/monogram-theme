3c95bac623b1dd6423d656c982e8b7f6abea0192
cs <bartus.csongor@gmail.com>
2015-11-04 09:24:54 +0200

    Switching back to threejs cube

--- site/components/project/cube/cube.js
+++ site/components/project/cube/cube.js
@@ -1,42 +1,50 @@
-var scene, camera, renderer;
+var cubeRotate = function(containerID) {
+  var scene, camera, renderer;
 
-var WIDTH  = 300;
-var HEIGHT = 300;
+  var WIDTH  = 300;
+  var HEIGHT = 300;
 
-var SPEED = 0.01;
+  var SPEED = 0.01;
 
-var cubeInit = function(containerID) {
-  scene = new THREE.Scene();
+  var cubeInit = function(containerID) {
+    scene = new THREE.Scene();
 
-  // init cube
-  cube = new THREE.Mesh(new THREE.CubeGeometry(2, 2, 2), new THREE.MeshNormalMaterial());
-  scene.add(cube);
+    // init cube
+    cube = new THREE.Mesh(new THREE.CubeGeometry(2, 2, 2), new THREE.MeshNormalMaterial());
+    scene.add(cube);
 
-  // init camera
-  camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 10);
-  camera.position.set(0, 3.5, 5);
-  camera.lookAt(scene.position);
+    // init camera
+    camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 10);
+    camera.position.set(0, 3.5, 5);
+    camera.lookAt(scene.position);
 
-  // init renderer
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(WIDTH, HEIGHT);
+    // init renderer
+    renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true 
+    });
+    renderer.setSize(WIDTH, HEIGHT);
 
-  var container = document.querySelector(containerID);
-  container.appendChild(renderer.domElement);
-}
-
-
-var cubeRender = function() {
-  requestAnimationFrame(cubeRender);
-  rotateCube();
-  renderer.render(scene, camera);
-
-  function rotateCube() {
-    cube.rotation.x -= SPEED * 2;
-    cube.rotation.y -= SPEED;
-    cube.rotation.z -= SPEED * 3;
+    var container = document.querySelector(containerID);
+    container.appendChild(renderer.domElement);
   }
+
+
+  var cubeRender = function() {
+    requestAnimationFrame(cubeRender);
+    rotateCube();
+    renderer.render(scene, camera);
+
+    function rotateCube() {
+      cube.rotation.x -= SPEED * 2;
+      cube.rotation.y -= SPEED;
+      cube.rotation.z -= SPEED * 3;
+    }
+  }
+
+  cubeInit(containerID);
+  cubeRender();
 }
 
-cubeInit('.cube__container');
-cubeRender();
+
+cubeRotate('.cube__container');
